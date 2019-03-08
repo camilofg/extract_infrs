1f99394c406c1c46048f5c3afd303a6f50027986
Unknown <rc.forero@uniandes.edu.co>
2019-03-07 19:32:47 -0500

    adjusts for single value

--- main.py
+++ main.py
@@ -7,27 +7,40 @@ else:
     route_file = input('insert the file route')
 
 with open(route_file) as csv_file:
-    with open('C:\CF\Delta_Canalizaciones\generated_extract.csv', 'w+') as file_out:
+    with open('D:\codensa\generated_extract1.csv', 'w+') as file_out:
         line = csv_file.readline()
         counter = 0
         while line:
+            new_line = ''
             if counter > 0:
                 sp = line.split(';')
+                if sp[0] == '114793':
+                    print('punto interrupcion')
                 if sp.__len__() > 0:
-                    if sp[1] == 'MT' and sp[5] != 'unset':
+                    if sp[3] == 'MT' and sp[7] != 'unset':
                         try:
-                            sp_infr = sp[5].split('-')
+                            sp_infr = sp[7].split('-')
                             if sp_infr.__len__() > 1:
-                                infr_i = ''.join(re.findall('[\d*]', str(sp_infr[0])))#.split(':')[1]
-                                infr_f = ''.join(re.findall('[\d*]', str(sp_infr[1])))#str(sp_infr[1]).split(':')[1]
-                                ctos = '-'.join(set(re.findall('[A-Z]+[\d]+', sp[5])))
+                                infr_i = ''.join(re.findall('[\d*]', str(sp_infr[0])))
+                                infr_f = ''.join(re.findall('[\d*]', str(sp_infr[1])))
+                                ctos = '-'.join(set(re.findall('[A-Z]+[\d]+', sp[7])))
                                 new_line = '{};{};{};{}'.format(line, infr_i, infr_f, ctos)
+
+                            elif sp_infr.__len__() == 1:
+                                infr_i = ''.join(re.findall('[\d*]', str(sp_infr[0])))
+                                ctos = '-'.join(set(re.findall('[A-Z]+[\d]+', sp[7])))
+                                new_line = '{};{};{};{}'.format(line, infr_i, '', ctos)
+
+                            else:
+                                new_line = '{};{};{};{}'.format(line, '', '', '')
+                                
                         except Exception as error:
                             print(line)
                             print('line: ' + str(counter))
-                            print('line: ' + sp[5])
+                            print('line: ' + sp[7])
                             print(error)
-                            #print('infr_i: {} - infr_f: {} - cto: {}'.format(infr_i, infr_f, ctos))
+                    else:
+                        new_line = '{};{};{};{}'.format(line, '', '', '')
             else:
                 new_line = line+';INFR_I;INFR_F;CTOS'
             file_out.writelines(new_line.replace('\n', '')+'\n')
